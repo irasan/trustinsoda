82cd49cabc18af8cff13751fd09432e3e8453734
AwsSG <aws.sabah.gheni@gmail.com>
2021-09-11 14:14:22 +0000

    linked js files and added basic content

---
+++ static/js/chatbot_answers.js
@@ -0,0 +1,10 @@
+// this is a dictionary of all the labels for the input fields
+const labelList = {
+
+  };
+  
+// this is a dictionary of all the input fields that the user has to fill
+  const inputList = {
+
+  };
+  
