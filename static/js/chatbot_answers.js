f20f4a648e6d45cc5f15c5c60b3adc41b2fb1e21
AwsSG <aws.sabah.gheni@gmail.com>
2021-09-12 00:21:49 +0000

    added logic and updated answers for chatbot

--- static/js/chatbot_answers.js
+++ static/js/chatbot_answers.js
@@ -1,10 +1,14 @@
 // this is a dictionary of all the labels for the input fields
 const labelList = {
-
+    "full_name": "what is your full name?",
+    "email": "Nice name! Now, what is your email address?",
+    "phone": "Great! next is your phone number",
   };
   
 // this is a dictionary of all the input fields that the user has to fill
   const inputList = {
-
+    "full_name": '<input type="text" class="form-control current-input" id="full_name" name="full_name" required>',
+    "email": '<input type="email" class="form-control current-input" id="email" name="email" required>',
+    "phone": '<input type="text" class="form-control current-input" id="phone" name="phone" required>',
   };
   
--- static/js/chatbot_logic.js
+++ static/js/chatbot_logic.js
@@ -1,5 +1,30 @@
 $(document).ready(function () {
-    
-    $("h5").css('color', 'red');
 
+  $("h5").css('color', 'red');
+
+
+  $('.chatbox').keypress(function (e) {
+    var key = e.which;
+    if (key == 13) // the enter key code
+    {
+      $(".current-input").click();
+      let label = $(".current-input").attr("id");
+
+      var next = function(dict, key) {
+        var keys = Object.keys(dict)
+          , i = keys.indexOf(key);
+        return i !== -1 && keys[i + 1];
+      };
+
+      nextLabel = next(inputList, label)
+
+      $(".current-input").removeClass("current-input").addClass("previous-input");
+
+      $(".chatbox").append(inputList[nextLabel]);
+
+
+      return console.log(nextLabel);
+    }
   });
+
+});
